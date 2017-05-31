e415ec57b7e501930b137eb8936ed4244082f999
spidgorny <spidgorny@gmail.com>
2017-06-01 01:09:34 +0200

    - ADD searching for Z3

---
+++ configuration.php
@@ -0,0 +1,13 @@
+<?php
+
+use DTS\eBaySDK\Credentials\CredentialsProvider;
+
+$config['sandbox']['credentials'] = [
+	CredentialsProvider::ENV_APP_ID => 'SlawaPid-SearchHe-SBX-7dbc19de5-45bcbcc9',
+	CredentialsProvider::ENV_DEV_ID => '63e7a23e-a574-4452-a204-77d938f845b2',
+	CredentialsProvider::ENV_CERT_ID => 'SBX-dbc19de5199b-27b0-4dd4-b941-1f3c',
+	'appId' => 'SlawaPid-SearchHe-SBX-7dbc19de5-45bcbcc9',
+	'devId' => '63e7a23e-a574-4452-a204-77d938f845b2',
+	'certId' => 'SBX-dbc19de5199b-27b0-4dd4-b941-1f3c',
+];
+return $config;
